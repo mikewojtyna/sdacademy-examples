8bde1a6313ffc664ba3958b3b43904a14ab85658
Mike Wojtyna <mike@buildmysoftware.pro>
2018-06-10 12:59:51 +0200

    Fix maven project and compilation errors

--- 09_klasy_abstrakcyjne/src/main/java/pl/sdacademy/pizzakinds/ItalianPizza.java
+++ 09_klasy_abstrakcyjne/src/main/java/pl/sdacademy/pizzakinds/ItalianPizza.java
@@ -3,14 +3,18 @@ package pl.sdacademy.pizzakinds;
 
 public class ItalianPizza extends Pizza {
 
-    String dodatkowySer;
-    String zioła;
+	String dodatkowySer;
+	String zioła;
 
-    public ItalianPizza(int rozmiar, int ileSera, String dodatkowySer, String zioła) {
-        super(rozmiar, ileSera);
-        this.dodatkowySer = dodatkowySer;
-        this.zioła = zioła;
-    }
+	public ItalianPizza(int rozmiar, int ileSera, String dodatkowySer, String zioła) {
+		super(rozmiar, ileSera);
+		this.dodatkowySer = dodatkowySer;
+		this.zioła = zioła;
+	}
 
 
+	@Override
+	public void opiszSie() {
+		throw new UnsupportedOperationException("implement this");
+	}
 }
---
+++ 16_string/src/test/java/pl/sdacademy/testhomework/VovelsCounter.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.testhomework;
+
+public class VovelsCounter {
+	public int count(String text) {
+		throw new UnsupportedOperationException("implement this method");
+	}
+}
