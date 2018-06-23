b4891551dfe58ac9fd109a9485c6119ea9fc8f8e
Mike Wojtyna <mike@buildmysoftware.pro>
2018-06-23 22:55:28 +0200

    Bubble sorter step 14 - change bubble sorter to not modify the original list

--- 27-algorithms/src/main/java/pl/sdacademy/algorithms/sort/BubbleSorter.java
+++ 27-algorithms/src/main/java/pl/sdacademy/algorithms/sort/BubbleSorter.java
@@ -1,18 +1,20 @@
 package pl.sdacademy.algorithms.sort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class BubbleSorter implements Sorter {
 	@Override
 	public <T extends Comparable<T>> List<T> sort(List<T> input) {
+		ArrayList<T> sortedList = new ArrayList<>(input);
 		if (input.size() > 1) {
 			for (int i = 0; i < input.size() - 1; i++) {
 				for (int j = 0; j < input.size() - 1; j++) {
-					swapAdjacentElements(j, input);
+					swapAdjacentElements(j, sortedList);
 				}
 			}
 		}
-		return input;
+		return sortedList;
 	}
 
 	private <T extends Comparable<T>> void swapAdjacentElements(int currentIndex, List<T> list) {
