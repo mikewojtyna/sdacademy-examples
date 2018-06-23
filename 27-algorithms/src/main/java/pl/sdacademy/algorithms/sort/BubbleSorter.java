70ba396e063f84e5aebf031152e1a778647e359b
Mike Wojtyna <mike@buildmysoftware.pro>
2018-06-23 22:06:16 +0200

    Write bubble sorter step 7

--- 27-algorithms/src/main/java/pl/sdacademy/algorithms/sort/BubbleSorter.java
+++ 27-algorithms/src/main/java/pl/sdacademy/algorithms/sort/BubbleSorter.java
@@ -7,7 +7,11 @@ class BubbleSorter implements Sorter {
 	@Override
 	public <T extends Comparable<T>> List<T> sort(List<T> input) {
 		if (input.size() > 1) {
-			return Arrays.asList(input.get(1), input.get(0));
+			T firstElement = input.get(0);
+			T secondElement = input.get(1);
+			if (firstElement.compareTo(secondElement) > 0) {
+				return Arrays.asList(secondElement, firstElement);
+			}
 		}
 		return input;
 	}
