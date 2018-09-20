0d037e80caeddcf235235466cdf6f0facd7a9096
Mike Wojtyna <mike@buildmysoftware.pro>
2018-09-20 09:51:04 +0200

    Add default transaction isolation level explicitly in twitter 5 scripts

--- db/src/main/resources/dbs/twitter/5/twitter-5.sql
+++ db/src/main/resources/dbs/twitter/5/twitter-5.sql
@@ -1,3 +1,13 @@
+/*
+REPEATABLE_READ is the default isolation level in InnoDB - the default and most commonly used database engine in
+MySQL. Since its a default value, we don't need to set it here explicitly. However, it's a good idea to leave it here
+during the course to make it easier to understand.
+
+In this example we set the isolation level only for the next transaction. Refer to the official documentation to see
+all available scopes https://dev.mysql.com/doc/refman/8.0/en/set-transaction.html.
+*/
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
 -- 1. Start transaction
 START TRANSACTION;
 
