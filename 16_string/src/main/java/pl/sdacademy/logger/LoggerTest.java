7e2dcef62f20f13bf689c927aa32a66f9e1aa796
Mike Wojtyna <mike@buildmysoftware.pro>
2018-12-16 09:12:22 +0100

    Update logger example in string module to use slf4j

--- 16_string/src/main/java/pl/sdacademy/logger/LoggerTest.java
+++ 16_string/src/main/java/pl/sdacademy/logger/LoggerTest.java
@@ -1,13 +1,14 @@
 package pl.sdacademy.logger;
 
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoggerTest {
-    static Logger logger = Logger.getLogger(LoggerTest.class.getName());
+	static Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
-    public static void main(String[] args) {
-        logger.info("This is info log");
-        logger.debug("This is debug message");
-    }
+	public static void main(String[] args) {
+		logger.info("This is info log");
+		logger.debug("This is debug message");
+	}
 }
