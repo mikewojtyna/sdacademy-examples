fe350bf861563f367333362b7835d3bdd574a44f
Mike Wojtyna <mike@buildmysoftware.pro>
2019-02-03 09:52:46 +0100

    Add JDBC data source example

--- db/src/test/java/pl/sdacademy/db/jdbc/JdbcHelloWorldTest.java
+++ db/src/test/java/pl/sdacademy/db/jdbc/JdbcHelloWorldTest.java
@@ -1,8 +1,10 @@
 package pl.sdacademy.db.jdbc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,7 +13,7 @@ import java.sql.Statement;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class JdbcHelloWorldTest {
-	@DisplayName("show how to connect to database using jdbc")
+	@DisplayName("show how to connect to database using jdbc driver manager")
 	@Test
 	void test() throws Exception {
 		try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:"); Statement statement =
@@ -21,8 +23,31 @@ public class JdbcHelloWorldTest {
 			ResultSet resultSet = statement.executeQuery("SELECT * FROM my_table");
 			resultSet.next();
 			String name = resultSet.getString("name");
-			
+
 			assertThat(name).isEqualTo("goobar");
 		}
+	}
+
+	@DisplayName("show how to connect to database using jdbc data source")
+	@Test
+	void test1() throws Exception {
+		DataSource dataSource = createH2DataSource();
+
+		try (Connection connection = dataSource.getConnection(); Statement statement =
+			connection.createStatement()) {
+			statement.execute("CREATE TABLE my_table(id int auto_increment, name varchar(30))");
+			statement.execute("INSERT INTO my_table(name) VALUES('goobar')");
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM my_table");
+			resultSet.next();
+			String name = resultSet.getString("name");
+
+			assertThat(name).isEqualTo("goobar");
+		}
+	}
+
+	private DataSource createH2DataSource() {
+		JdbcDataSource dataSource = new JdbcDataSource();
+		dataSource.setUrl("jdbc:h2:mem:");
+		return dataSource;
 	}
 }
